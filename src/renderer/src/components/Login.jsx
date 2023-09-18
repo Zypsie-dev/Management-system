9ad54a89738ba483fc4fc69bd241a215cfd3564b
Zypsie-dev <9860224200rs@gmail.com>
2023-09-18 19:51:45 +0545

    add login

--- src/renderer/src/components/Login.jsx
+++ src/renderer/src/components/Login.jsx
@@ -1,3 +1,59 @@
+import { useState } from "react"
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+  Button,
+} from "@material-tailwind/react";
 function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+    <Card className="w-96">
+      <CardHeader
+        variant="gradient"
+        color="gray"
+        className="mb-4 grid h-28 place-items-center"
+      >
+        <Typography variant="h3" color="white">
+          Sign In
+        </Typography>
+      </CardHeader>
+      <CardBody className="flex flex-col gap-4">
+        <Input label="Username" size="lg" onChange={(e) => setUsername(e.target.value)} />
+        <Input label="Password" size="lg" onChange={(e) => setPassword(e.target.value)} />
+        <div className="-ml-2.5">
+          <Checkbox label="Remember Me" />
+        </div>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Button variant="gradient" fullWidth  onClick={() => {
+                                console.log("Logging in...")
+                                window.ipcRender.send('login',{username, password})
+        }}>
+          Sign In
+        </Button>
+        <Typography variant="small" className="mt-6 flex justify-center">
+          Don&apos;t have an account?
+          <Typography
+            as="a"
+            href="#signup"
+            variant="small"
+            color="blue-gray"
+            className="ml-1 font-bold"
+          >
+            Sign up
+          </Typography>
+        </Typography>
+      </CardFooter>
+    </Card>
+  );
     
 }
+export default Login;
+
+                            
